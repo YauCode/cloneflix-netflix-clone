9c5f0f42ce83db9b87397d67d80a6372bc3b858b
YauCode <yauheni.baikou88@gmail.com>
2023-06-26 20:03:07 +0300

    serverAuth fix

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-import prosmadb from '@/lib/prismadb'
+import prismadb from '@/lib/prismadb'
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
