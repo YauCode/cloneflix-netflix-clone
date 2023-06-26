7ea1e8e121c019d17a54b861b05d6c085d9f2131
YauCode <yauheni.baikou88@gmail.com>
2023-06-26 19:08:29 +0300

    Done

---
+++ components/NavbarItem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface NavbarItemProps {
+    label: string;
+    active?: boolean;
+}
+
+const NavbarItem: React.FC<NavbarItemProps> = ({
+    label
+}) => {
+    return (
+        <div className="text-white hover:text-gray-300 cursor-pointer transition">
+            {label}
+        </div>
+    )
+}
+
+export default NavbarItem;
