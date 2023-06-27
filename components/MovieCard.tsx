bfe335e78364b13a47bd6a4268a85e6ce526a889
YauCode <yauheni.baikou88@gmail.com>
2023-06-27 11:51:04 +0300

    MovieCard Title added

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -80,6 +80,9 @@ const MovieCard: React.FC<MovieCardProps> = ({
                             <AiOutlineInfo className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
                         </div>
                     </div>
+                    <div className="flex flex-row mt-4 gap-2 font-semibold items-center">
+                        <p className="text-white text-[10px] lg:text-sm">{data.title}</p>
+                    </div>
                     <p className="text-green-400 font-semibold mt-4">
                         New <span className="text-white">2023</span>
                     </p>
